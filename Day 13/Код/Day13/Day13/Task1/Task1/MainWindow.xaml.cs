c4109b1b0aea62e6f73a60d0e346da282ea9bb46
Admin <Admin@CFGPC>
2025-03-31 11:56:20 +0300

    день 12

---
+++ Day 13/Код/Day13/Day13/Task1/Task1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> Products { get; set; }
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            
+            Products = new ObservableCollection<Product>();
+            ProductsDataGrid.ItemsSource = Products;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+      
+            var product = new Product { Name = "Новый товар", Quantity = 1, Price = 0.0 };
+            Products.Add(product);
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+            if (ProductsDataGrid.SelectedItem is Product selectedProduct)
+            {
+                selectedProduct.Name = "Редактированный товар"; 
+
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+         
+            if (ProductsDataGrid.SelectedItem is Product selectedProduct)
+            {
+                Products.Remove(selectedProduct);
+            }
+        }
+    }
+}
