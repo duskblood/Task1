1470ee6456c44f6c63a2d00003dac1cf3285afc7
Admin <Admin@CFGPC>
2025-04-03 09:04:58 +0300

    день 15

---
+++ Day 16/Код/Day16/Day16/Task1/Task1/WarehouseService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class WarehouseService
+    {
+        public async Task<List<ProductModel>> LoadProductsAsync()
+        {
+            await Task.Delay(2000); 
+            return new List<ProductModel>
+        {
+            new ProductModel { Id = 1, Name = "Товар 1", Quantity = 10 },
+            new ProductModel { Id = 2, Name = "Товар 2", Quantity = 5 }
+        };
+        }
+    }
+}
---
+++ Day 16/Код/Day16/Day16/Task1/Task1/WarehouseViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Task1
+{
+    internal class WarehouseViewModel : INotifyPropertyChanged
+    {
+        private readonly WarehouseService _warehouseService;
+        private bool _isLoading;
+
+        public ObservableCollection<ProductModel> Products { get; set; }
+        public ICommand LoadProductsCommand { get; }
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set
+            {
+                _isLoading = value;
+                OnPropertyChanged(nameof(IsLoading));
+            }
+        }
+
+        public WarehouseViewModel()
+        {
+            _warehouseService = new WarehouseService();
+            Products = new ObservableCollection<ProductModel>();
+            LoadProductsCommand = new RelayCommand(async () => await LoadProductsAsync(), () => !IsLoading);
+        }
+
+        private async Task LoadProductsAsync()
+        {
+            IsLoading = true;
+            var products = await _warehouseService.LoadProductsAsync();
+            Products.Clear();
+            foreach (var product in products)
+            {
+                Products.Add(product);
+            }
+            IsLoading = false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
